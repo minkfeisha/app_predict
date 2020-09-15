f9dea61b2dd1a4da27eee9c218da08579e84b642
minkfeisha <71218302+minkfeisha@users.noreply.github.com>
2020-09-15 18:12:03 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Predictive Health</title>
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
 
+</head>
+<body>  
+    <h1>Demo Predictive Health Version 1</h1>
+    <h2><a href="https://stroke-prediction.herokuapp.com">Stroke Disease</a></h2>
+    <h2><a href="https://hheart-prediction.herokuapp.com">Ischemic Heart Disease</a></h2>
+    <h2><a href="https://rheumatic-prediction.herokuapp.com">Rheumatic Heart Disease</a></h2>
+</body>
+</html>
